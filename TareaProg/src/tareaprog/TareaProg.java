e36a5039a2053f3c909e7fd8b1fde244fba3a93f
daninonino <danielanovoao600@gmail.com>
2022-09-27 11:37:20 -0300

    intento2

--- TareaProg/src/tareaprog/TareaProg.java
+++ TareaProg/src/tareaprog/TareaProg.java
@@ -1,20 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tareaprog;
 
-/**
- *
- * @author danino
- */
 public class TareaProg {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
     }
     
+}
+
+class OrdenCompra{
+    private Date fecha;
+    private String estado;
+    
+    public OrdenDeCompra(){}
+    
+    public calcPrecioSinIVA(){}
+    
+    public calcPeso(){}
+}
+
+class DetalleOrden{
+    private int cantidad;
+    
+    public DetalleOrden(){}
+    
+    public calcPrecio();
+    
+    public calcPrecioSinIVA(){}
+    
+    public calcPeso(){}
+    
+}
+
+class Articulo{
+    private float peso;
+    private String nombre;
+    
+    public Articulo(){}
+
 }
